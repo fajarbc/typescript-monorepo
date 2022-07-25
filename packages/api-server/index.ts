6bf234055f682d2b0cff869f15c2aa6db1211e69
Fajar BC <bcfajar@gmail.com>
2022-07-25 23:45:53 +0700

    Implement Basic Chat Message : - Input of user & message (useState), onClick trpc/addMessage - Implemented using invalidateQueries, auto fetch trpc/getMessages

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,17 +1,48 @@
 import express from "express";
 import * as trpc from "@trpc/server"
 import * as trpcExpress from "@trpc/server/adapters/express"
+import cors from "cors"
+import { z } from "zod"
+
+interface chatMessage {
+  user: string;
+  message: string;
+}
+
+const messages: chatMessage[] = [
+  {user: "Alice", message: "Hello Bob"},
+  {user: "Bob", message: "Hi Alice"},
+];
 
 const appRouter = trpc.router()
 .query("hello", {
   resolve() {
-    return "Hello World";
+    return "Hello World!";
   },
 })
+.query("getMessages", {
+  input: z.number().default(10),
+  resolve({input}) {
+    return messages.slice(-input)
+  }
+})
+.mutation("addMessage", {
+  input: z.object({
+    user: z.string(),
+    message: z.string(),
+  }),
+  resolve({input}) {
+    messages.push(input)
+    return input
+  }
+})
+
+export type AppRouter = typeof appRouter
 
 const app = express();
 const port = 8080;
 
+app.use(cors())
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { trpc } from "./trpc";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: client</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+const client = new QueryClient();
+
+const AppContent = () => {
+  const getMessages = trpc.useQuery(["getMessages"]);
+
+  const [user, setUser] = useState("")
+  const [message, setMessage] = useState("")
+  const addMessage = trpc.useMutation(["addMessage"]);
+  const onAddMessage = () => {
+    addMessage.mutate({
+      user: user,
+      message: message,
+    },
+    {
+      onSuccess: () => {
+        client.invalidateQueries(["getMessages"])
+      }
+    })
+  }
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>{ (getMessages.data ?? []).map(row => (
+        <div key={row.message} className="m-2">
+          <span className="text-blue-500 font-bold">{row.user}</span>: {row.message}
+        </div>
+      )) }</div>
+
+      <div className="mt-10">
+        <input 
+          type="text"
+          value={user}
+          onChange={(e) => setUser(e.target.value)}
+          placeholder="User"
+          className="p-4 border border-gray 500 rounded-md w-full"
+        />
+        <input 
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Message"
+          className="p-4 border border-gray 500 rounded-md w-full"
+        />
+      </div>
+      <button 
+        onClick={onAddMessage}
+        className="bg-blue-500 text-white p-4 rounded-full"
+      >Add Message</button>
+    </div>
+  );
+};
+
+const App = () => {
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      url: "http://localhost:8080/trpc",
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={client}>
+    <QueryClientProvider client={client}>
+      <AppContent/>
+    </QueryClientProvider>
+    </trpc.Provider>
+  )
+}
+
 ReactDOM.render(<App />, document.getElementById("app"));
